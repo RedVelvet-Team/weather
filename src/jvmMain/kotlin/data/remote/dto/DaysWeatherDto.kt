84ab0bdf665e58c95be23d09a7a13ee82748e40d
Haidy <haidyabugom3a@gmail.com>
2023-07-12 23:02:42 +0300

    add forecast dto

--- src/jvmMain/kotlin/data/remote/dto/DaysWeatherDto.kt
+++ src/jvmMain/kotlin/data/remote/dto/DaysWeatherDto.kt
@@ -5,6 +5,6 @@ package data.remote.dto
 @kotlinx.serialization.Serializable
 data class DaysWeatherDto(
     val current: CurrentDto?,
-    val forecast: ForecastDayDto?,
+    val forecast: ForecastDto?,
     val location: LocationDto?
 )
