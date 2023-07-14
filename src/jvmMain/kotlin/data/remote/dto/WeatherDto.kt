e874972c25b8fc0c217adc5295fdc2b45411d82b
Mahmoud Akram Al-Mallahi <nalmamoud303@gmail.com>
2023-07-14 21:44:46 +0300

    create WeatherUIState.kt

--- src/jvmMain/kotlin/data/remote/dto/WeatherDto.kt
+++ src/jvmMain/kotlin/data/remote/dto/WeatherDto.kt
@@ -1,31 +1,101 @@
 package data.remote.dto
 
-import com.google.gson.annotations.SerializedName
+
+import kotlinx.serialization.SerialName
 
 
+data class WeatherDTO(
+    @SerialName("query")
+    val query: Query
+) {
 
-data class WeatherDto(
-    val current: CurrentDto?,
-    val location: LocationDto?
-)
+    data class Query(
+        @SerialName("custom_id")
+        val customId: String,
+        @SerialName("q")
+        val q: String,
+        @SerialName("location")
+        val location: Location,
+        @SerialName("current")
+        val current: Current
+    ) {
 
+        data class Location(
+            @SerialName("name")
+            val name: String,
+            @SerialName("region")
+            val region: String,
+            @SerialName("country")
+            val country: String,
+            @SerialName("lat")
+            val lat: Double,
+            @SerialName("lon")
+            val lon: Double,
+            @SerialName("tz_id")
+            val tzId: String,
+            @SerialName("localtime_epoch")
+            val localtimeEpoch: Int,
+            @SerialName("localtime")
+            val localtime: String
+        )
 
-data class ConditionDto(
-    val code: Int?,
-    val icon: String?,
-    val text: String?
-)
+        data class Current(
+            @SerialName("last_updated_epoch")
+            val lastUpdatedEpoch: Int,
+            @SerialName("last_updated")
+            val lastUpdated: String,
+            @SerialName("temp_c")
+            val tempC: Double,
+            @SerialName("temp_f")
+            val tempF: Double,
+            @SerialName("is_day")
+            val isDay: Int,
+            @SerialName("condition")
+            val condition: Condition,
+            @SerialName("wind_mph")
+            val windMph: Double,
+            @SerialName("wind_kph")
+            val windKph: Double,
+            @SerialName("wind_degree")
+            val windDegree: Int,
+            @SerialName("wind_dir")
+            val windDir: String,
+            @SerialName("pressure_mb")
+            val pressureMb: Double,
+            @SerialName("pressure_in")
+            val pressureIn: Double,
+            @SerialName("precip_mm")
+            val precipMm: Double,
+            @SerialName("precip_in")
+            val precipIn: Double,
+            @SerialName("humidity")
+            val humidity: Int,
+            @SerialName("cloud")
+            val cloud: Int,
+            @SerialName("feelslike_c")
+            val feelslikeC: Double,
+            @SerialName("feelslike_f")
+            val feelslikeF: Double,
+            @SerialName("vis_km")
+            val visKm: Double,
+            @SerialName("vis_miles")
+            val visMiles: Double,
+            @SerialName("uv")
+            val uv: Double,
+            @SerialName("gust_mph")
+            val gustMph: Double,
+            @SerialName("gust_kph")
+            val gustKph: Double
+        ) {
 
-
-data class LocationDto(
-    val country: String?,
-    val lat: Double?,
-    val localtime: String?,
-    @SerializedName("localtime_epoch")
-    val localtimeEpoch: Int?,
-    val lon: Double?,
-    val name: String?,
-    val region: String?,
-    @SerializedName("tz_id")
-    val tzId: String?
-)
+            data class Condition(
+                @SerialName("text")
+                val text: String,
+                @SerialName("icon")
+                val icon: String,
+                @SerialName("code")
+                val code: Int
+            )
+        }
+    }
+}
