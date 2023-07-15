87d5ccc9a81644c90c3a3f4a41cc842920c65e89
Najeia Tammous <ntammous11@gmail.com>
2023-07-15 22:10:43 +0300

    first creation for home screen

---
+++ src/jvmMain/kotlin/presentation/HomeScreen.kt
@@ -0,0 +1,60 @@
+package presentation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+@Composable
+fun HomeScreen() {
+    Box(Modifier.fillMaxSize()) {
+
+        Image(
+            painter = painterResource(
+                "desktop.jpg"), contentDescription = null, modifier = Modifier.fillMaxSize(), contentScale = ContentScale.FillBounds
+        )
+        Box(
+            modifier = Modifier.fillMaxHeight().width(300.dp).background(Color(0x80C4C4C4))
+                .clipToBounds()
+        ) {
+            Column(Modifier.fillMaxSize().padding(horizontal = 40.dp)) {
+                Row(Modifier.fillMaxWidth().padding(top = 40.dp), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.End) {
+
+                    Text(text = "Search", fontSize =  14.sp , color = Color.White, textAlign = TextAlign.Center)
+                    Spacer(Modifier.width(16.dp))
+                    Icon(painter = painterResource("Magnifer.svg"),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp),
+                        tint = Color.White
+                    )
+                }
+                Divider(
+                    modifier = Modifier
+                        .fillMaxWidth().padding(top = 24.dp)
+                        .width(1.dp),
+                    color = Color(0x999FFFFFF)
+                )
+            }
+        }
+    }
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        HomeScreen()
+    }
+}
