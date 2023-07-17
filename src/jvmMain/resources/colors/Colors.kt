0be7065fe0262fb7b96123a46fd64b2c29ec445f
kamel <kikokeko10@gmail.com>
2023-07-17 17:34:48 +0300

    add font and color

---
+++ src/jvmMain/resources/colors/Colors.kt
@@ -0,0 +1,9 @@
+package colors
+
+import androidx.compose.ui.graphics.Color
+
+val White87 = Color(0xDEFFFFFF)
+
+val White60 = Color(0x99FFFFFF)
+
+val White38 = Color(0x66FFFFFF)
---
+++ src/jvmMain/resources/fonts/Fonts.kt
@@ -0,0 +1,19 @@
+package fonts
+
+
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.platform.Font
+
+val RubikBold = FontFamily(
+    Font("fonts/rubik.ttf", FontWeight(500), FontStyle.Normal)
+)
+
+val RubikSemiBold = FontFamily(
+    Font("fonts/rubik.ttf", FontWeight(400), FontStyle.Normal)
+)
+
+val RubikBlack = FontFamily(
+    Font("fonts/rubik.ttf", FontWeight(600), FontStyle.Normal)
+)
